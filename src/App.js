badc5a9112d696998d1868032d470b7034010fc6
Ram <rbpata19@gmail.com>
2023-08-13 12:37:47 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
+// import User from './User_func';
+import User from './user_class';
+// or 
+// import React,{Component} from 'react'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
+      <User/>
+      <User/>
+      <User/>
     </div>
   );
 }
+
+// function User() { //you can use that way also
+//   return (
+//     <div className="App">
+//       <h1>hello Users!!</h1>
+//     </div>
+//   );
+// }
+
+// ->>this way we can also write class component
+// class User extends Component{
+
+//   render(){
+//       return(
+//           <h1>HEllo User</h1>
+//       )
+//   }
+
+// }
 
 export default App;
---
+++ src/User_func.js
@@ -0,0 +1,10 @@
+function User() {
+    return (
+      <div className="App">
+        <h1>hello Users!!</h1>
+      </div>
+    );
+  }
+  
+  export default User;
+  
