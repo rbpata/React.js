6956167c046bca167979fad9b31f56eed21c1a5b
Ram <rbpata19@gmail.com>
2023-08-13 12:56:18 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
+  const [status, setStatus] = React.useState(false);
+
   return (
     <div className="App">
-      <h1>hello world</h1>
-      <button></button>
+      {
+        status?
+      <h1>hello world</h1>: null
+}
+      <button onClick={()=>setStatus(true)}>Show</button>
+      <button onClick={()=>setStatus(false)}>Hide</button>
     </div>
   );
 }
