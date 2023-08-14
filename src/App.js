67bf73f3b95551af35a1e635a9ac6d1c6a7ee782
Ram <rbpata19@gmail.com>
2023-08-14 16:26:02 +0530

    Bootstrap_basic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
+import React from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
 function App() {
+  let arr = ["ram","mihir"];
+  arr.map((item)=>{
+
+    console.log(item);
+
+  })
   return (
     <div className="App">
       <h1>hello world</h1>
+
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+      <Button as="input" type="button" value="Input" />{' '}
+      <Button as="input" type="submit" value="Submit" />{' '}
+
+   
+
     </div>
   );
 }
-
 
 export default App;
